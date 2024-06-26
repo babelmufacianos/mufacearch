16e337f3852830e3824d9dd6a0199e20b4a435a9
pedro.dulce <pedro.dulce@gmail.com>
2024-06-26 17:08:56 +0200

    Integrado comportamiento AOP en DilpmaAPI y vía application.yml en ClienteAPI

--- src/main/java/muface/application/usecases/api/DiplomaAPI.java
+++ src/main/java/muface/application/usecases/api/DiplomaAPI.java
@@ -15,43 +15,43 @@ public class DiplomaAPI extends ArqBaseRestController {
     @ArqUseCaseDefinition(value = "CrearDiplomaUseCase", type = ArqUseCaseType.CREATE)
     @PostMapping
     public ResponseEntity<Object> crear(@RequestBody DiplomaDTO dtoInBody) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
     @ArqUseCaseDefinition(value = "ActualizarDiplomaUseCase", type = ArqUseCaseType.UPDATE)
     @PutMapping
     public ResponseEntity<Object> actualizar(@RequestBody DiplomaDTO dtoInBody) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
     @ArqUseCaseDefinition(value = "BorrarDiplomasUseCase", type = ArqUseCaseType.DELETE)
     @DeleteMapping
     public ResponseEntity<Object> borrar(@RequestBody DiplomaDTO dtoInBody) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
     @ArqUseCaseDefinition(value = "BorrarDiplomaPorIdUseCase", type = ArqUseCaseType.DELETE_BY_ID)
     @DeleteMapping("{id}")
     public ResponseEntity<Object> borrarPorId(@PathVariable Long id) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
     @ArqUseCaseDefinition(value = "ConsultaPorIdDiplomasUseCase", type = ArqUseCaseType.QUERY_BY_ID)
     @GetMapping("{id}")
     public ResponseEntity<Object> consultaPorId(@PathVariable Long id) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
     @ArqUseCaseDefinition(value = "ConsultasDiplomasUseCase", type = ArqUseCaseType.QUERY_BY_PARAMS)
     @PostMapping("consulta")
     public ResponseEntity<Object> consultaPorCampos(@RequestBody DiplomaDTO filter) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
     @ArqUseCaseDefinition(value = "ConsultasPaginadasDiplomasUseCase", type = ArqUseCaseType.QUERY_PAGINATED)
     @PostMapping("consultapaginados")
     public ResponseEntity<Object> consultaPaginadaPorCampos(@RequestBody DiplomaDTO filter, Pageable pageable) {
-        return null; // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
+        return ResponseEntity.ok(""); // MÃ©todo vacÃ­o, lÃ³gica manejada por el aspecto
     }
 
 }

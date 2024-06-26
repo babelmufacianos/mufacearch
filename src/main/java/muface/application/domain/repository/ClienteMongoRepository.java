459e87a9fe8d81fb4f144a693cffc641efd6e21d
pedro.dulce <pedro.dulce@gmail.com>
2024-06-26 17:37:59 +0200

    Integrado comportamiento AOP en DilpmaAPI y vía application.yml en ClienteAPI

--- src/main/java/muface/application/domain/repository/ClienteMongoRepository.java
+++ src/main/java/muface/application/domain/repository/ClienteMongoRepository.java
@@ -3,13 +3,13 @@ package muface.application.domain.repository;
 import muface.application.domain.model.ClienteDocument;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ClienteMongoRepository extends PagingAndSortingRepository<ClienteDocument, String>, ClienteRepository {
+public interface ClienteMongoRepository extends MongoRepository<ClienteDocument, String>, ClienteRepository {
 
         List<ClienteDocument> findByOfficial(String titulacion);
 

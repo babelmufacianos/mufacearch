4f97130d9b375c6c6927c0fbf5b263eb2375c0e6
pedro.dulce <pedro.dulce@gmail.com>
2024-06-28 23:23:46 +0200

    Mejoras y simplificación de Archrepository

--- src/main/java/muface/application/domain/repository/clientes/ClienteMongoRepository.java
+++ src/main/java/muface/application/domain/repository/clientes/ClienteMongoRepository.java
@@ -1,12 +1,8 @@
 package muface.application.domain.repository.clientes;
 
 import muface.application.domain.model.ClienteDocument;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ClienteMongoRepository extends MongoRepository<ClienteDocument, String>, ClienteRepository {

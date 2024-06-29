4ac20c677decfef82a9d82127a402434ebac3488
pedro.dulce <pedro.dulce@gmail.com>
2024-06-29 20:19:32 +0200

    Mejoras y simplificación de Archrepository

--- src/main/java/muface/application/domain/repository/clientes/ClienteRepository.java
+++ src/main/java/muface/application/domain/repository/clientes/ClienteRepository.java
@@ -4,13 +4,17 @@ import muface.application.domain.model.ClienteDocument;
 import muface.arch.repository.ArqRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface ClienteRepository extends ArqRepository<ClienteDocument, String> {
+@Repository
+public interface ClienteRepository extends MongoRepository<ClienteDocument, String>, ArqRepository<ClienteDocument, String> {
 
     List<ClienteDocument> findByOfficial(String titulacionName);
 
     Page<ClienteDocument> findByOfficial(String titulacionName, Pageable pageable);
 
 }
+
--- src/main/java/muface/application/domain/repository/diplomas/DiplomaRepository.java
+++ src/main/java/muface/application/domain/repository/diplomas/DiplomaRepository.java
@@ -4,10 +4,13 @@ import muface.application.domain.model.Diploma;
 import muface.arch.repository.ArqRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-public interface DiplomaRepository extends ArqRepository<Diploma, Long> {
-
+@Repository
+public interface DiplomaRepository extends JpaRepository<Diploma, Long>, ArqRepository<Diploma, Long> {
     List<Diploma> findDiplomasByTitulo(String titulo);
 
     Page<Diploma> findDiplomasByTitulo(String titulo, Pageable pageable);
